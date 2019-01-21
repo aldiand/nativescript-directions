1c486d8ee39c00ca75a37e8d1186e4ee62d97f09
Dhika <dhikasma4@gmail.com>
2019-01-21 17:24:02 +0700

    newEditProfile

--- app/components/login/EditProfile.vue
+++ app/components/login/EditProfile.vue
@@ -1,33 +1,48 @@
 <template>
     <Page class="page">
-        <ActionBar class="action-bar2">
+        <ActionBar class="action-bar2" backgroundColor="#03c1b8">
             <StackLayout orientation="horizontal">
-                <Label :text="'sign_in'|L" fontSize="24" verticalAlignment="center" />
+                <Label :text="'Edit Profile'|L" fontSize="24"
+                    verticalAlignment="center" />
             </StackLayout>
         </ActionBar>
         <ScrollView>
-            <StackLayout class="parent-container">
-                <StackLayout orientation="vertical">
-                    <Label text="Nama Depan" />
-                    <TextField v-model="namaDepan" />
-                </StackLayout>
-                <StackLayout orientation="vertical">
-                    <Label text="Nama Belakang" />
-                    <TextField v-model="namaBelakang" />
-                </StackLayout>
-                <StackLayout orientation="vertical">
-                    <Label text="Jenis Kelamin" />
-                    <ListPicker :items="listPickerGender" v-model="selectedGenderIndex" />
-                </StackLayout>
-                <StackLayout orientation="vertical">
-                    <Label text="Lokasi" />
-                    <ListPicker :items="listPickerLocation" v-model="selectedLocationIndex" />
-                </StackLayout>
-                <StackLayout orientation="vertical">
-                    <Label text="Email" />
-                    <TextField v-model="email" />
-                </StackLayout>
-            </StackLayout>
+                    <StackLayout class="parent-container" marginTop="15"
+                        marginLeft="15" marginRight="15" marginBottom="15">
+                        <StackLayout orientation="vertical" marginTop="15"
+                            marginLeft="15" marginRight="15" marginBottom="15">
+                            <Label text="Nama Depan" />
+                            <TextField v-model="namaDepan" />
+                        </StackLayout>
+                        <StackLayout orientation="vertical" marginTop="15"
+                            marginLeft="15" marginRight="15" marginBottom="15">
+                            <Label text="Nama Belakang" />
+                            <TextField v-model="namaBelakang" />
+                        </StackLayout>
+                        <StackLayout orientation="vertical" marginTop="15"
+                            marginLeft="15" marginRight="15" marginBottom="15">
+                            <Label text="Jenis Kelamin" />
+                            <ListPicker :items="listPickerGender" v-model="selectedGenderIndex" />
+                        </StackLayout>
+                        <StackLayout orientation="vertical" marginTop="15"
+                            marginLeft="15" marginRight="15" marginBottom="15">
+                            <Label text="Lokasi" />
+                            <ListPicker :items="listPickerLocation" v-model="selectedLocationIndex" />
+                        </StackLayout>
+                        <StackLayout orientation="vertical" marginTop="15"
+                            marginLeft="15" marginRight="15" marginBottom="15">
+                            <Label text="Email" />
+                            <TextField v-model="email" />
+                        </StackLayout>
+                        <StackLayout style="margin-top:30;">
+                            <Button text="Selesai" @tap="onSubmit" class="app-btn btn btn-primary"
+                                v-bind:visibility="busy ? 'collapse': 'visible'"
+                                backgroundColor="#03c1b8">
+                            </Button>
+                            <ActivityIndicator class="activity-indicator"
+                                v-bind:busy="busy"></ActivityIndicator>
+                        </StackLayout>
+                    </StackLayout>
         </ScrollView>
     </Page>
 </template>
@@ -51,24 +66,3 @@
 
 <style scoped>
 </style>
-
-
-<script>
-import App from '../App'
-export default {
-  data() {
-    return {
-    };
-  },
-
-  mounted() {
-      this.goToMain();
-  },
-
-  methods: {
-      goToMain() {
-        this.$navigateTo(EditProfile, { clearHistory:true });
-      }
-  }
-};
-</script>
