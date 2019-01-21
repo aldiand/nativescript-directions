4c77d2f33d049d41f4443ab81263313e8489c5dc
Andre Bahtiar Fauzi <supersandre@gmail.com>
2019-01-22 00:30:12 +0700

    getting ready for new backlog

---
+++ app/components/mydoctor/Maps.vue
@@ -0,0 +1,14 @@
+
+<script>
+export default {
+    props: {
+        title: String,
+        address: String,
+        longitude: Number,
+        latitude: Number
+    },
+    methods: {
+        
+    },
+};
+</script>
