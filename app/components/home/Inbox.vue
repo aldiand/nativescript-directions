adff605641b34e05827ea27b1cdc9b668362b9c5
Andre Bahtiar Fauzi <supersandre@gmail.com>
2019-01-23 17:01:14 +0700

    save

--- app/components/home/Inbox.vue
+++ app/components/home/Inbox.vue
@@ -1,6 +1,41 @@
 <template>
     <StackLayout orientation="vertical" width="100%">
         <Label text="Inbox" textWrap="true" class="text-title"/>
+        <ListView for="item in inboxs">
+            <v-template>
+                <InboxList :item="item"/>
+            </v-template>
+        </ListView>
         
     </StackLayout>
 </template>
+
+<script>
+import InboxList from "./InboxList";
+
+export default {
+  components: {
+    InboxList
+  },
+  data() {
+    return {
+      inboxs: []
+    };
+  },
+
+  mounted() {
+    this.$http.get(
+      "/appointments",
+      content => {
+        let responsePayload = content.content;
+        this.appointments = responsePayload;
+      },
+      error => {
+
+        }
+    );
+  },
+
+  methods: {}
+};
+</script>
---
+++ app/components/home/InboxList.vue
@@ -0,0 +1,30 @@
+<template>
+    <StackLayout>
+        <Label :text="item.doctor_name" class="text-primary"/>
+        <Label :text="item.clinic_name" class="text-primary"/>
+        <Label :text="item.doctor_photo" class="text-primary"/>
+        <Label :text="item.clinic_photo" class="text-primary"/>
+        <Label :text="item.title" class="text-primary"/>
+    </StackLayout>
+</template>
+
+<script>
+export default {
+    props: {
+        item : {
+            doctor_id: Number,
+            clinic_id: Number,
+            clinic: String,
+            message: String,
+            title: String,
+            id: Number,
+            doctor_name: String,
+            doctor_photo: String,
+            clinic_name: String,
+            clinic_photo: String,
+            created_at: String,
+            to_patient_desc: String,
+        }
+    }
+};
+</script>
