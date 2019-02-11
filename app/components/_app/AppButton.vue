08df14b8f5b7dbec1836e8a78d77171e8ef6d1f9
Andre Bahtiar Fauzi <supersandre@gmail.com>
2019-02-11 17:31:04 +0700

    add button

---
+++ app/components/_app/AppButton.vue
@@ -0,0 +1,12 @@
+<template>
+    <Button :text="text" class="app-btn btn btn-primary"
+    style="border-radius:24px;font-size:18pt"></Button>
+</template>
+
+<script>
+export default {
+    props:{
+        text: ""
+    }
+}
+</script>
--- app/modules/component.js
+++ app/modules/component.js
@@ -1,7 +1,9 @@
 import Vue from 'nativescript-vue'
 import AppBar from '../components/_app/AppBar'
+import AppButton from '../components/_app/AppButton'
 
 
 export function setUpComponent(){
     Vue.component('AppBar', AppBar);
+    Vue.component('AppButton', AppButton);
 }
