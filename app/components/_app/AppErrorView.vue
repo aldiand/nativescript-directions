95249dd30cdda8897780e7447428259d4703b3c1
Andre Bahtiar Fauzi <supersandre@gmail.com>
2019-02-18 00:07:28 +0700

    save

---
+++ app/components/_app/AppErrorView.vue
@@ -0,0 +1,15 @@
+<template>
+  <StackLayout orientation="vertical" width="100%" height="100%" 
+    horizontalAlignment="center" verticalAlignment="center">
+    <Label :text="text" class="text-danger" style="text-align:center"></Label>
+    
+  </StackLayout>
+</template>
+
+<script>
+export default {
+    props: {
+        text: ""
+    }
+}
+</script>
