1e5771cab07d088834450d2003ee8b073e677c5a
Andre Bahtiar Fauzi <supersandre@gmail.com>
2019-02-22 14:27:30 +0700

    save list

--- app/components/home/InboxList.vue
+++ app/components/home/InboxList.vue
@@ -1,12 +1,11 @@
 <template>
     <StackLayout>
         <DockLayout class="container-list">
-        <Image :src="item.clinic_photo" class="text-primary" style="width:150px;height:150px;margin:5px;" />
-        <Image :src="item.doctor_photo" class="text-primary" style="width:150px;height:150px;margin:5px;" />
+        <Image :src="item.doctor_photo? item.doctor_photo : item.clinic_photo" class="text-primary" style="width:150px;height:150px;margin:5px;" />
             <StackLayout dock="left" orientation="vertical" style="padding:15px;" horizontalAlignment="stretch">
                 <Label :text="item.clinic_name == '' ? item.doctor_name : item.clinic_name" style="font-weight:bold;font-size:14pt;margin-top:12px"/>
-                <Label :text="item.message" class="text-primary" style="font-size:12pt;margin-top:8px"/>
-                <Label :text="item.title" class="text-primary" visibility="collapse"/>        
+                <!-- <Label :text="item.message" class="text-primary" style="font-size:12pt;margin-top:8px"/> -->
+                <Label :text="item.title" class="text-primary"/>        
             </StackLayout>
         <Image dock="right" src="~/assets/images/next-doctor-profile.png" class="next-btn" horizontalAlignment="right" />
         </DockLayout>
