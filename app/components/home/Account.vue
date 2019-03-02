dc69f551994fde3c3fc427793c060411cc4a97d4
Danah <butuhdanah@gmail.com>
2019-03-02 15:08:54 +0700

    UI for account

--- app/components/home/Account.vue
+++ app/components/home/Account.vue
@@ -1,6 +1,36 @@
 <template>
-    <StackLayout orientation="vertical" width="100%">
+    <StackLayout orientation="vertical" width="100%" >
         <Label text="Account" textWrap="true" class="text-title"/>
-        
+        <DockLayout class="container-list">
+            <Image src="~/assets/images/user.png" class="image-list" />           
+            <Image dock="right" src="~/assets/images/next-doctor-profile.png" class="next-btn" horizontalAlligment="right" />
+            <label :text="'fragment_settings_account'|L" textWrap="true" style="font-size:12pt;margin-top:50px;margin-left:20px" horizontalAlligment="stretch" /> 
+        </DockLayout>
+        <DockLayout class="container-list">
+            <Image src="~/assets/images/info.png" class="image-list" />
+            <Image dock="right" src="~/assets/images/next-doctor-profile.png" class="next-btn" horizontalAlligment="right" />
+            <label :text="'fragment_settings_about'|L" textWrap="true" style="font-size:12pt;margin-top:50px;margin-left:20px" horizontalAlligment="stretch"/>
+        </DockLayout>
+        <DockLayout class="container-list">
+                <Image src="~/assets/images/star.png" class="image-list" />
+                <Image dock="right" src="~/assets/images/next-doctor-profile.png" class="next-btn" horizontalAlligment="right" />
+                <label :text="'fragment_settings_rate'|L" textWrap="true" style="font-size:12pt;margin-top:50px;margin-left:20px" horizontalAlligment="stretch"/>
+        </DockLayout>
     </StackLayout>
 </template>
+
+<style>
+    .container-list{
+        width: 100%;
+        padding:10px;   
+        background : #ffffff
+    }
+    .next-btn {
+        width: 30px;
+        margin: 10px;
+    }
+    .image-list{
+        width: 85px;
+        margin: 20px;
+    }
+</style>
