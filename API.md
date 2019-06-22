bab9ff0d99b5124e944600c56f94c14c8f2fbfb9
Andre Bahtiar Fauzi <supersandre@gmail.com>
2019-06-23 02:49:27 +0700

    save

---
+++ API.md
@@ -0,0 +1,18 @@
+# API
+
+> Readydok Mobile
+
+## Currently Using
+
+* PATCH /user/{id}
+* DELETE /appointments/{id}
+* GET /appointments/{id}
+* GET /bookings/{id}
+* POST /bookings
+* POST /appointments/{id}/reschedule
+* GET /reminders/{id}
+* GET /recall/{id}
+* GET /messages/{id}
+* DELETE /messages/{id}
+* POST /messages/{id}/reply
+* GET /search?query=
---
+++ app/modules/readydok_api.js
@@ -0,0 +1,74 @@
+/**
+ *
+ *  Example:
+ *
+ *  import ReadydokAPI, { BASE_URL, BASE_API_VERSION, BASE_API_NAME } from './readydok_api';
+ *
+ *  const _api = new ReadydokAPI();
+ *
+ *  function test(id, success, error) {
+ *      _api.call('GET', `appointments/${id}`, success, error);
+ *      // or
+ *      _api.version('v1.1').call('GET', `appointments/${id}`, success, error);
+ *  }
+ *
+ */
+
+import * as store from './store'
+const axios = require('axios');
+
+export const BASE_URL = "https://readydok.com/api/";
+export const BASE_API_VERSION = "v1";
+export const BASE_API_NAME = "/android/";
+
+export default class ReadydokAPI {
+    constructor() {
+        this.api_version = BASE_API_VERSION;
+        this.header = {
+            'Authorization': 'Bearer ' + store.get(store.TOKEN, ''),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        };
+    }
+
+    version(version) {
+        this.api_version = version ?? BASE_API_VERSION;
+        return this;
+    }
+
+    resetVersion() {
+        this.api_version = BASE_API_VERSION;
+        return this;
+    }
+
+    refreshHeader() {
+        this.header = Object.assign({}, this.header, {
+            'Authorization': 'Bearer ' + store.get(store.TOKEN, '')
+        });
+        return this;
+    }
+
+    generateFullUrl(uri) {
+        return `${BASE_URL}${this.api_version}${BASE_API_NAME}`;
+    }
+
+    call(method, uri, data, success, error) {
+        this.refreshHeader();
+
+        axios({
+            method: method,
+            "url": this.generateFullUrl(uri),
+            "data": data,
+            "headers": this.header
+        }).then(res => {
+            if (res.status)
+                success(res);
+            else 
+                error("Network Error");
+        }).catch(err => {
+            error(err.response);
+        });
+
+        this.resetVersion();
+    }
+}
